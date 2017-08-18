f4aec32a316b16419faa833ced0d26442a3b9b79
Ruslan Mustafin <ruslanmust87@gmail.com>
2017-08-18 18:52:46 +0500

    Исправления

--- js/script.js
+++ js/script.js
@@ -9,3 +9,36 @@ document.querySelector('.fullscreen-menu__close').addEventListener('click', func
 });
 
 });
+
+//слайдер
+
+var button = $('.slider__arrow-img'),
+    container = button.closest('.slider__block');
+
+button.on('click', function(){
+    var $this = $(this),
+        items = $('.slider__item', container),
+        itemActive = items.filter('.slider__item-active'),
+        list = $('.slider__list'),
+        itemSib;
+
+        if ($this.hasClass('slider__arrow-right')) {
+            itemSib = itemActive.next();
+            if (itemSib.length == 0) {
+                itemSib = items.first();
+            };
+        } else {
+            itemSib = itemActive.prev();
+            if (itemSib.length == 0) {
+                itemSib = items.last();
+            };
+        };
+        
+    var itemIndex = itemSib.index();
+
+        list.animate({'left': (itemIndex * -100) + '%'}, 500, function(){
+            itemActive.removeClass('slider__item-active');
+            itemSib.addClass('slider__item-active');
+        });
+
+});
