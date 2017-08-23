826f274480d9a305ea33c4449b8ff336d6df3e92
Ruslan <ruslanmust87@gmail.com>
2017-08-24 02:24:50 +0500

    Карта

--- js/script.js
+++ js/script.js
@@ -151,3 +151,44 @@ $(window).swipe({
         scrollDirection(direction);
     }
 })
+
+//карта
+
+ymaps.ready(init);
+var myMap;
+
+function init(){     
+    myMap = new ymaps.Map("map", {
+        center: [59.91817154482064,30.30557799999997],
+        zoom: 12,
+        controls: [],
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    var myPlacemark = new ymaps.Placemark([], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: '../image/icons/map-marker.svg',
+            iconImageSize: [50, 72],
+            iconImageOffset: [-3, -42]
+        });
+    // myMap.geoObjects.add(myPlacemark);
+    var coords = [
+        [59.94554327989287,30.38935262114668],
+        [59.91142323563909,30.50024587065841],
+        [59.88693161784606,30.319658102103713],
+        [59.97033574821672,30.315194906302924]
+    ],
+        myCollection = new ymaps.GeoObjectCollection({}, {
+            iconLayout: 'default#image',
+            iconImageHref: '../image/icons/map-marker.svg',
+            iconImageSize: [50, 72],
+            iconImageOffset: [-3, -42]
+        });
+    
+    for (var i = 0; i < coords.length; i++) {
+        myCollection.add(new ymaps.Placemark(coords[i]));
+    }
+    
+    myMap.geoObjects.add(myCollection);
+}
