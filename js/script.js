19f0c973426e1cef039e34662e3e91b0623c62e4
Ruslan <ruslanmust87@gmail.com>
2017-08-23 09:57:11 +0500

    OnePageScroll

--- js/script.js
+++ js/script.js
@@ -42,3 +42,91 @@ button.on('click', function(){
         });
 
 });
+
+//аккордеон
+
+var acco = $('.acco'),
+    accoItems = $('.acco__item'),
+    contentHeight;
+
+accoItems.on('click', function(){
+    var $this = $(this),
+        accoTitle = $('.acco__title', this),
+        accoContent = $('.acco__content', this);
+
+        // if (screen.width > 768) {
+        //     contentHeight = 100
+        // } else {
+        //     contentHeight = 170
+        // }
+
+        // if ($this.hasClass('acco__item-active')) { 
+        //     accoContent.animate({'height': '0'}, {duration: 500,  queue: false});
+        //     $this.removeClass('acco__item-active');
+        // } else {
+        //     accoItems.removeClass('acco__item-active');
+        //     $this.addClass('acco__item-active');
+        // // accoTitle.css('color', '#f9b43b');
+        //     accoItems.find('.acco__content').animate({'height': '0'}, {duration: 500, queue: false});
+        //     accoContent.animate({'height': contentHeight + 'px'}, {duration: 500, queue: false});
+        // }
+
+        if ($this.hasClass('acco__item-active')) { 
+            accoContent.slideUp(500);
+            $this.removeClass('acco__item-active');
+        } else {
+            accoItems.removeClass('acco__item-active');
+            $this.addClass('acco__item-active');
+        // accoTitle.css('color', '#f9b43b');
+            accoItems.find('.acco__content').slideUp(500);
+            accoContent.slideDown(500)
+        }
+})
+
+var accoVertical = $('.acco-vertical'),
+    accoVerItems = $('.acco-vertical__item');
+
+accoVerItems.on('click', function(){
+    var $this = $(this),
+        accoVerItemContent = $('.acco-vertical__content',this);
+    
+    if ($this.hasClass('acco-vertical__item-active')) { 
+        accoVerItemContent.animate({'width': '0'}, {duration: 500, queue: false});
+        $this.removeClass('acco-vertical__item-active');
+    } else {
+        accoVerItems.removeClass('acco-vertical__item-active');
+        $this.addClass('acco-vertical__item-active');
+        // accoVerItems.animate({'width': '0'}, {duration: 500, queue: false});
+        accoVerItems.find('.acco-vertical__content').animate({'width': '0'}, {duration: 500, queue: false});
+        // $this.animate({'width': '100%'}, {duration: 500, queue: false});
+        accoVerItemContent.animate({'width': '100%'}, {duration: 500, queue: false});
+    }
+})
+
+$('.wrapper').on('wheel', function(event){
+
+    var main = $('.main'),
+        sections = $('.section'),
+        sectionActive = sections.filter('.section-active'),
+        sectionNext = sectionActive.next(),
+        sectionPrev = sectionActive.prev(),
+        position;
+
+    sectionActive.removeClass('section-active');
+        
+    var scrollMain = function(position){
+        main.css('transform', 'translateY(' + position + '%)');
+    }
+    
+    if (event.originalEvent.deltaY > 0 && sectionNext.length) {
+        position = (sectionNext.index() * -100);
+        sectionNext.addClass('section-active');
+    }
+        
+    if (event.originalEvent.deltaY < 0 && sectionPrev.length) {
+        position = (sectionPrev.index() * 100);
+        sectionPrev.addClass('section-active');
+    }
+
+    scrollMain(position);
+})
